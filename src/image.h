d3bcd5b1df7bde1bf096f833124b2bb3a85a8075
Bradly Landucci <salvesoundsofficial@gmail.com>
2026-01-24 14:30:38 -0800

    Make spritesheets from spritesheet masks

--- src/image.h
+++ src/image.h
@@ -11,8 +11,17 @@
 #include <iostream>
 #include <string>
 
+#define ASSERT(condition, message)                                                                                     \
+    {                                                                                                                  \
+        if (!(condition))                                                                                              \
+        {                                                                                                              \
+            std::cerr << message << std::endl;                                                                         \
+        }                                                                                                              \
+    }
+
 constexpr uint32_t DESIRED_CHANNELS{ 4 };
-const std::string INPUT_DIR{ "input" };
+const std::string GROUND_INPUT_DIR{ "ground_input" };
+const std::string PATH_INPUT_DIR{ "path_input" };
 const std::string OUTPUT_DIR{ "output" };
 const std::string TEMPLATE_DIR{ "templates" };
 
@@ -67,6 +76,70 @@ struct Image
                             in.height, in.channels, in.data, in.width * in.channels))
         {
             std::cerr << "Failed to write\n";
+        }
+    }
+
+    static void createPathFromSpriteSheet(const Image &ground, const Image &path, const Image &maskSheet)
+    {
+        if (ground.filename[0] == '.' || path.filename[0] == '.' || maskSheet.filename[0] == '.')
+        {
+            return;
+        }
+
+        ASSERT((ground.channels == path.channels) && (ground.channels == maskSheet.channels),
+               "All images must have the same amount of channels");
+        ASSERT(maskSheet.width % ground.width == 0, "Mask sheet must be multiple of ground");
+        ASSERT(ground.width == path.width, "Ground and path images must have the same width");
+        ASSERT((ground.channels == path.channels) && (ground.channels == maskSheet.channels),
+               "All images must have the same height");
+
+        uint32_t groundWidth{ (uint32_t)ground.width };
+        uint32_t height{ (uint32_t)ground.height };
+
+        uint32_t numTiles{ maskSheet.width / groundWidth };
+        std::vector<uint8_t> outData(maskSheet.width * height * maskSheet.channels, 0);
+        for (uint32_t t = 0; t < numTiles; t++)
+        {
+            uint32_t tileWidthBytes{ groundWidth * maskSheet.channels };
+            for (uint32_t y = 0; y < height; y++)
+            {
+                for (uint32_t x = 0; x < groundWidth; x++)
+                {
+                    uint32_t groundPixelOffsetBytes{ (y * groundWidth + x) * ground.channels };
+                    uint32_t maskPixelOffset{ (y * maskSheet.width + x + (t * groundWidth)) * maskSheet.channels };
+
+                    uint8_t r{ *(maskSheet.data + maskPixelOffset) };
+                    uint8_t g{ *(maskSheet.data + maskPixelOffset + 1) };
+                    uint8_t b{ *(maskSheet.data + maskPixelOffset + 2) };
+                    bool isMask{ r == 0 && g == 0 && b == 0 };
+
+                    uint8_t *tileDataToUse{};
+                    if (isMask)
+                    {
+                        tileDataToUse = ground.data;
+                    }
+                    else
+                    {
+                        tileDataToUse = path.data;
+                    }
+
+                    if (maskPixelOffset < (maskSheet.width * maskSheet.height * maskSheet.channels))
+                    {
+                        outData[maskPixelOffset] = tileDataToUse[groundPixelOffsetBytes];
+                        outData[maskPixelOffset + 1] = tileDataToUse[groundPixelOffsetBytes + 1];
+                        outData[maskPixelOffset + 2] = tileDataToUse[groundPixelOffsetBytes + 2];
+                        outData[maskPixelOffset + 3] = tileDataToUse[groundPixelOffsetBytes + 3];
+                    }
+                }
+            }
+        }
+        if (!stbi_write_png((OUTPUT_DIR + "/" + ground.filename + "_" + path.filename + "_" + maskSheet.filename +
+                             "_spritesheet.png")
+                                .c_str(),
+                            maskSheet.width, maskSheet.height, maskSheet.channels, outData.data(),
+                            maskSheet.width * maskSheet.channels))
+        {
+            std::cerr << "Failed to write" << std::endl;
         }
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,25 @@
 
 int main()
 {
-    for (const auto &inFile : std::filesystem::directory_iterator{ INPUT_DIR })
+    for (const auto &inFile : std::filesystem::directory_iterator{ GROUND_INPUT_DIR })
     {
         std::string inFilename{ inFile.path().filename().string() };
         Image inputImage{ inFile.path() };
         for (const auto &templateFile : std::filesystem::directory_iterator{ TEMPLATE_DIR })
         {
             Image templateImage{ templateFile.path() };
-            Image::createMaskedImage(inputImage, templateImage);
+            if (templateImage.width > inputImage.width)
+            {
+                for (const auto &pathFile : std::filesystem::directory_iterator{ PATH_INPUT_DIR })
+                {
+                    Image pathImage{ pathFile.path() };
+                    Image::createPathFromSpriteSheet(inputImage, pathImage, templateImage);
+                }
+            }
+            else
+            {
+                Image::createMaskedImage(inputImage, templateImage);
+            }
         }
     }
 
