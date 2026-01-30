f510d9a150411aff99e87fbf7c5215169a94d043
Bradly Landucci <salvesoundsofficial@gmail.com>
2026-01-29 22:22:32 -0800

    Added noise configuration

--- src/ui/editors/noiser.cpp
+++ src/ui/editors/noiser.cpp
@@ -2,7 +2,8 @@
 #include <ui/components/image_frame.h>
 #include <ui/models/selectable_file_list_model.h>
 #include <ui/utilities/theme.h>
-#include <processing/image_manipulation.h>
+
+#include <magic_enum/magic_enum.hpp>
 
 //-------------------------------------------------------------------------------------------------//
 
@@ -25,8 +26,35 @@ Noiser::Noiser()
                                                       (void)p_caller;
                                                       return dropViewChanged(tree);
                                                   }));
+
+    m_noiseTypeCombo.addItem("Value", 1);
+    m_noiseTypeCombo.addItem("Perlin", 2);
+    m_noiseTypeCombo.addItem("Cellular", 3);
+    m_noiseTypeCombo.addItem("Simplex", 4);
+    m_noiseTypeCombo.setSelectedId(1);
+
+    m_opacitySlider.setRange(0.0, 1.0, 0.01);
+    m_frequencySlider.setRange(0.0, 100.0, 0.1);
+    m_seedSlider.setRange(0.0, 10'000.0, 1.0);
+
+    auto forceUpdateCb = [this]()
+    {
+        juce::String selected{ getSelectedInput() };
+        updateOutput(selected);
+    };
+
+    m_noiseTypeCombo.onChange = [forceUpdateCb]() { forceUpdateCb(); };
+    m_opacitySlider.onValueChange = [forceUpdateCb]() { forceUpdateCb(); };
+    m_frequencySlider.onValueChange = [forceUpdateCb]() { forceUpdateCb(); };
+    m_seedSlider.onValueChange = [forceUpdateCb]() { forceUpdateCb(); };
+
     addAndMakeVisible(*mp_inputs);
     addAndMakeVisible(m_output);
+
+    addAndMakeVisible(m_noiseTypeCombo);
+    addAndMakeVisible(m_opacitySlider);
+    addAndMakeVisible(m_frequencySlider);
+    addAndMakeVisible(m_seedSlider);
 
     m_tree.addListener(this);
 }
@@ -37,6 +65,20 @@ void Noiser::resized()
 {
     auto bounds{ getLocalBounds() };
     int halfWidth{ bounds.getWidth() / 2 };
+
+    auto bottom{ bounds.removeFromBottom(100) };
+
+    int fourthWidth{ bottom.getWidth() / 4 };
+
+    auto bottomLeft{ bottom.removeFromLeft(fourthWidth) };
+    auto bottomMiddleLeft{ bottom.removeFromLeft(fourthWidth) };
+    auto bottomMiddleRight{ bottom.removeFromLeft(fourthWidth) };
+    auto bottomRight{ bottom };
+
+    m_noiseTypeCombo.setBounds(bottomLeft);
+    m_opacitySlider.setBounds(bottomMiddleLeft);
+    m_frequencySlider.setBounds(bottomMiddleRight);
+    m_seedSlider.setBounds(bottomRight);
 
     auto left{ bounds.removeFromLeft(halfWidth) };
     auto right{ bounds };
@@ -68,26 +110,63 @@ void Noiser::valueTreePropertyChanged(juce::ValueTree &treeWhosePropertyHasChang
 {
     if (property == Theme::SELECTED_KEY && treeWhosePropertyHasChanged[property])
     {
-        juce::String inputFilename{ treeWhosePropertyHasChanged.getType().toString() };
+        juce::String input{ treeWhosePropertyHasChanged.getType().toString() };
 
-        ImageFrame *p_output{ m_output.getComponent<ImageFrame>() };
+        updateOutput(input);
+    }
+}
 
-        p_output->reset();
-        ImageData inputImageData{ inputFilename.toStdString() };
+//-------------------------------------------------------------------------------------------------//
 
-        /// TODO: These should be configurable through UI
-        float opacity{ 0.25f };
-        Noise::Type type{ Noise::Type::VALUE };
-        float frequency{ 50.f };
-        int seed{ 0 };
+juce::String Noiser::getSelectedInput()
+{
+    juce::String selected;
 
-        Noise noise{ type, frequency, seed };
+    juce::ValueTree inputTree{ m_tree.getChildWithName(INPUT_TREE_ID) };
 
-        std::unique_ptr<ImageData> p_noisedImage{ ImageManipulation::applyNoise(inputImageData, noise, opacity) };
-        if (p_noisedImage)
+    for (auto child : inputTree)
+    {
+        if (child.getProperty(Theme::SELECTED_KEY))
         {
-            p_output->setImage(std::move(p_noisedImage));
+            selected = child.getType().toString();
         }
+    }
+
+    return selected;
+}
+
+//-------------------------------------------------------------------------------------------------//
+
+Noise Noiser::getSelectedNoise()
+{
+    int selectedId{ m_noiseTypeCombo.getSelectedId() - 1 };
+    std::string selectedText{ m_noiseTypeCombo.getItemText(selectedId).toStdString() };
+    std::transform(selectedText.begin(), selectedText.end(), selectedText.begin(),
+                   [](auto c) { return std::toupper(c); });
+
+    Noise::Type type{ magic_enum::enum_cast<Noise::Type>(selectedText).value() };
+    float frequency{ (float)m_frequencySlider.getValue() };
+    int seed{ (int)m_seedSlider.getValue() };
+
+    return Noise{ type, frequency, seed };
+}
+
+//-------------------------------------------------------------------------------------------------//
+
+void Noiser::updateOutput(const juce::String &input)
+{
+    ImageFrame *p_output{ m_output.getComponent<ImageFrame>() };
+
+    p_output->reset();
+
+    ImageData inputImageData{ input.toStdString() };
+    Noise noise{ getSelectedNoise() };
+    float opacity{ (float)m_opacitySlider.getValue() };
+
+    std::unique_ptr<ImageData> p_noisedImage{ ImageManipulation::applyNoise(inputImageData, noise, opacity) };
+    if (p_noisedImage)
+    {
+        p_output->setImage(std::move(p_noisedImage));
     }
 }
 
@@ -95,7 +174,23 @@ void Noiser::valueTreePropertyChanged(juce::ValueTree &treeWhosePropertyHasChang
 
 void Noiser::generate(const juce::String &baseOutputDirectory)
 {
-    (void)baseOutputDirectory;
+    juce::ValueTree inputTree{ m_tree.getChildWithName(INPUT_TREE_ID) };
+
+    Noise noise{ getSelectedNoise() };
+    float opacity{ (float)m_opacitySlider.getValue() };
+
+    for (const auto &inImage : inputTree)
+    {
+        ImageData inputImageData{ inImage.getType().toString().toStdString() };
+        std::unique_ptr<ImageData> p_noisedImage{ ImageManipulation::applyNoise(inputImageData, noise, opacity) };
+        if (p_noisedImage)
+        {
+            p_noisedImage->filepath = baseOutputDirectory.toStdString() + "/" + inputImageData.filename + "/";
+            p_noisedImage->filename = inputImageData.filename + "_noised";
+
+            p_noisedImage->writeToDisk();
+        }
+    }
 }
 
 //-------------------------------------------------------------------------------------------------//
--- src/ui/editors/noiser.h
+++ src/ui/editors/noiser.h
@@ -3,6 +3,7 @@
 #include <ui/components/image_drop_view.h>
 #include <ui/components/titled_component.h>
 #include <ui/editors/editor.h>
+#include <processing/image_manipulation.h>
 
 //-------------------------------------------------------------------------------------------------//
 
@@ -22,10 +23,19 @@ private:
     void valueTreePropertyChanged(juce::ValueTree &treeWhosePropertyHasChanged,
                                   const juce::Identifier &property) override;
 
+    juce::String getSelectedInput();
+    Noise getSelectedNoise();
+    void updateOutput(const juce::String &input);
+
     juce::ValueTree m_tree;
 
     TitledComponent m_output;
     std::unique_ptr<TitledComponent> mp_inputs;
+
+    juce::ComboBox m_noiseTypeCombo;
+    juce::Slider m_opacitySlider;
+    juce::Slider m_frequencySlider;
+    juce::Slider m_seedSlider;
 };
 
 //-------------------------------------------------------------------------------------------------//
